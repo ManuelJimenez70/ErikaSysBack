242d272018f009630417e412e106bbc9146c43af
Manuel Jiménez <manuel.jimenez01@uptc.edu.co>
2023-08-25 20:38:47 -0500

    Correccion CORS

--- IdentityProvaider/Program.cs
+++ IdentityProvaider/Program.cs
@@ -39,6 +39,17 @@ builder.Services.AddScoped<RoleServices>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ProductService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "ERKOrigins",
+        configurePolicy: builder =>
+        {
+            builder.AllowAnyOrigin() // Permite todas las direcciones
+                   .AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .AllowCredentials();
+        });
+});
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(opt =>
@@ -60,7 +71,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("ERKOrigins");
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
